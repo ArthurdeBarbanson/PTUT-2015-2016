392d2d0518de8a61ef348161988fe22ee086a670
ArthurdeBarbanson <arthurdebarbanson@gmail.com>
2016-03-29 17:07:22 +0200

    SIte vide

--- src/SiteBundle/Controller/SecurityController.php
+++ src/SiteBundle/Controller/SecurityController.php
@@ -1,7 +1,28 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: arthur
- * Date: 29/03/16
- * Time: 16:29
- */
+namespace SiteBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class SecurityController extends Controller
+{
+    public function loginAction(Request $request)
+    {
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render(
+            'security/login.html.twig',
+            array(
+                // last username entered by the user
+                'last_username' => $lastUsername,
+                'error'         => $error,
+            )
+        );
+    }
+}
