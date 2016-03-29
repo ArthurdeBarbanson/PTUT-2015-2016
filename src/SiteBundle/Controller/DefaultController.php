dcef2b7422aceeb8c26fc4a4ab39f5bcba667865
Darkey01 <rey.0001@hotmail.fr>
2016-03-29 15:30:44 +0200

    ajout

--- src/SiteBundle/Controller/DefaultController.php
+++ src/SiteBundle/Controller/DefaultController.php
@@ -2,7 +2,9 @@
 
 namespace SiteBundle\Controller;
 
+use SiteBundle\Forms\Types\CreateAnnonce;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
@@ -11,9 +13,36 @@ class DefaultController extends Controller
         return $this->render('SiteBundle:Default:index.html.twig');
     }
 
-    public function ajoutAnnonceAction(){
+    public function ajoutAnnonceAction(Request $request){
+        {
+            $form = $this->createForm(CreateAnnonce::class);
 
-        return $this->render('SiteBundle:Default:ajoutAnnonce.html.twig');
+            if ($request->isMethod('post')) {
+                $form->submit($request);
+                if ($form->isValid()) {
+
+                    $data = $form->getData();
+
+                    $annonce= new Annonce(
+                        uniqid(),
+                        $data['name'],
+                        $data['description'],
+                        $data['price']
+                    );
+
+                    $this->container->get('product_repository')->save($product);
+
+                    $this->addFlash('info', "Le produit a bien été ajouté.");
+
+                    return $this->redirect('/');
+                }
+            }
+
+            return $this->render(
+                'MetinetECommerceBundle:Default:addProduct.html.twig',
+                ['form' => $form->createView()]
+            );
+        }
     }
 
 
