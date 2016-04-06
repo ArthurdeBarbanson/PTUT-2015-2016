517491c6836747972df92597f13601a1ac25b9b6
ArthurdeBarbanson <arthurdebarbanson@gmail.com>
2016-04-06 15:29:10 +0200

    ajout inscription entreprise

---
+++ src/SiteBundle/Forms/Types/AdresseType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace SiteBundle\Forms\Types;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AdresseType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('adresse')
+            ->add('codePostal')
+            ->add('commune')
+            ->add('pays')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'SiteBundle\Entity\Adresse'
+        ));
+    }
+}
