c961d394e508c8159575f6fb7eb08352e9fcb469
ArthurdeBarbanson <arthurdebarbanson@gmail.com>
2016-05-31 18:23:25 +0200

    maj etudiant

---
+++ src/SiteBundle/Forms/Types/SMTPType.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: arthur
+ * Date: 31/05/16
+ * Time: 16:59
+ */
+
+namespace SiteBundle\Forms\Types;
+
+
+use Symfony\Component\Form\AbstractType;
+
+class SMTPType extends AbstractType
+{
+
+}
