47bee7aaf9799ff9e7ad2919286f58342c25c7bc
Ludolive97419 <ludolive97419@gmail.com>
2016-05-31 09:51:54 +0200

    accueil etudiant

--- src/SiteBundle/Entity/EtudiantOffre.php
+++ src/SiteBundle/Entity/EtudiantOffre.php
@@ -48,6 +48,12 @@ class EtudiantOffre
      */
     private $Offre;
 
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="etat", type="string", columnDefinition="enum('Accepter','Refuser','Attente Entreprise','Attente Etudiant')" ,nullable=true)
+     */
+    private $etat;
 
 
 
@@ -152,5 +158,29 @@ class EtudiantOffre
     public function getOffre()
     {
         return $this->Offre;
+    }
+
+    /**
+     * Set etat
+     *
+     * @param string $etat
+     *
+     * @return EtudiantOffre
+     */
+    public function setEtat($etat)
+    {
+        $this->etat = $etat;
+
+        return $this;
+    }
+
+    /**
+     * Get etat
+     *
+     * @return string
+     */
+    public function getEtat()
+    {
+        return $this->etat;
     }
 }
