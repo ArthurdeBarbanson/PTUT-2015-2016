7458f1d521376b9a78db0f3f112505294e72c8cf
antoine <tou-toine@hotmail.fr>
2016-06-07 14:37:20 +0200

    gerer etudiant admissible

---
+++ src/SiteBundle/Forms/Types/EntretienType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace SiteBundle\Forms\Types;
+
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EntretienType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('commentaire', TextareaType::class, [
+            ])
+            ->add('datePassage', DateTimeType::class, [
+                'label' => 'Date de passage',
+                'data' => new \DateTime("now"),
+            ])
+            ->add('submit', SubmitType::class, ['label' => "S'inscrire", 'attr' => ['class' => 'btn-primary center-block']]);;
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'SiteBundle\Entity\Entretien'
+        ));
+    }
+}
