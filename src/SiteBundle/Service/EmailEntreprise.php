7c24652b440a97295134e7d2cd469a920f3b48fb
ArthurdeBarbanson <arthurdebarbanson@gmail.com>
2016-06-08 21:01:36 +0200

    upload piece jointe

---
+++ src/SiteBundle/Service/EmailEntreprise.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: arthur
+ * Date: 08/06/16
+ * Time: 20:57
+ */
+
+namespace SiteBundle\Service;
+
+
+class EmailEntreprise extends Email
+{
+    public function inscriptionEntreprise($adresseMail,$password){
+        $mail = \Swift_Message::newInstance();
+
+        $mail
+            ->setFrom('noreply-suivilpmetinet@iutinfobourg.fr')
+            ->setTo($adresseMail)
+            ->setSubject("inscription entreprise")
+            ->setBody(
+                $this->templating->render(
+                    'Emails/ajoutEntreprise.html.twig', [
+                        'password' => $password
+                    ]
+                )
+            )
+            ->setContentType('text/html');
+
+        $this->mailer->send($mail);
+    }
+}
