69cbf3f13c1fcf271c4e3fcbe82df72ce3d50fbc
ArthurdeBarbanson <arthurdebarbanson@gmail.com>
2016-06-09 09:33:01 +0200

    service maj

--- src/SiteBundle/Service/EmailEtudiant.php
+++ src/SiteBundle/Service/EmailEtudiant.php
@@ -8,7 +8,6 @@
 
 namespace SiteBundle\Service;
 
-
 class EmailEtudiant extends Email
 {
     public function envoyerMailEtape($adresseMail,$etape){
@@ -22,7 +21,7 @@ class EmailEtudiant extends Email
             ->getDoctrine()
             ->getManager()
             ->getRepository('SiteBundle:PieceJointe');
-
+        $pieceJointe=$repositoryPiecejointe->findBy(['Etape'=>$etape]);
 
         $listeEmails = $repository->find(1);
         $body="Aucune eétape sélectionner !";
@@ -55,6 +54,12 @@ class EmailEtudiant extends Email
             ->setSubject("Avancement inscription pédagogique")
             ->setBody($body)
             ->setContentType('text/html');
+
+        if(!empty($pieceJointe)){
+            foreach($pieceJointe as $piecej){
+                $mail->attach($piecej);
+            }
+        }
 
         $this->mailer->send($mail);
     }
